c4a8adb63c45a4a256641359a29367dee18efa19
Grown1994Tree <1437725934@qq.com>
2020-08-29 08:58:40 +0800

    优化

--- js/showPic.js
+++ js/showPic.js
@@ -1,21 +1,14 @@
 function showPic(which_pic){
+    if(!document.getElementById) return false;
     var source=which_pic.getAttribute("href");
     var get_img=document.getElementById("placeholder");
-    get_img.setAttribute("src",source);
     var pic_text=which_pic.getAttribute("title");
     var description=document.getElementById("description");
-    // alert(description.nodeType);description是元素节点，所以nodeValue的值为空。
-    // alert(description.nodeValue);
-    // alert(description.childNodes[0].nodeType);
-    // alert(description.childNodes[0].nodeValue);
-    description.childNodes[0].nodeValue=pic_text;
-    
-}
-
-function showPicCount(){
-    var element_body=document.getElementsByTagName("body")[0];
-    var element_number=element_body.childNodes.length;
-    alert(element_number);
+    get_img.setAttribute("src",source);
+    if(description){
+        description.childNodes[0].nodeValue=pic_text;
+    }
+    return true;
 }
 
 function openUrl(which_url){
@@ -24,12 +17,12 @@ function openUrl(which_url){
 
 
 function prepare(){
+    if(!document.getElementsByTagName) return false;
     var element_a=document.getElementsByTagName("a");
     for(var i=0;i<element_a.length;i++){
         if(element_a[i].getAttribute("class")!="popup"){
             element_a[i].onclick=function(){
-                showPic(this);//不能传element_a[i]，这时i=element_a.length
-                return false;
+               return !showPic(this);  
             };
         }else{
             element_a[i].onclick=function(){
@@ -40,10 +33,17 @@ function prepare(){
     }
 }
 
-// window.onload=function(){
-//     prepare();
-//     showPicCount();
-// }
-
-window.onload=prepare();
+function addLoadEvent(fun){
+    var old_load=window.onload;
+    if(typeof window.onload!='function'){
+           window.onload=fun;
+    }else{
+           window.onload=function(){
+              old_load();
+              fun();
+           }
+    }
+}
+addLoadEvent(prepare);
+// window.onload=prepare;
 
